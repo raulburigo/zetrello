ea4a053d73b566e061e7d6c531c3b5984f850ab7
Raul Burigo <raul.burigo@gmail.com>
2020-11-18 20:07:20 -0300

    nova página inicial

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,9 @@
 function Home() {
-    return
-    <div>
-        <h1>Página Inicial</h1>
-    </div>
+    return (
+        <div>
+            <h1>Página Inicial</h1>
+        </div>
+    )
 }
 
 export default Home
